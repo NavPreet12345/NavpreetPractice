8d598b3ee65ac3f927ce05a559773ccdd1cf7fdc
Navpreet Kaur Jhajj <Navpreet Kaur Jhajj@DESKTOP-89PPA7O>
2020-02-19 18:50:11 -0600

    2nd

--- src/program1/Program1.java
+++ src/program1/Program1.java
@@ -15,8 +15,8 @@ public class Program1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-      int a;
-        int b;
+      int a = 0;
+        int b = 0;
      
     int sum = a+b;
     
